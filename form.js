02b16ea69d9b4b1be7ecb1d81ea56732f83c88fb
Neha <nbelwalb21@gmail.com>
2019-10-02 21:25:51 +0530

    first time added this repository

---
+++ form.js
@@ -0,0 +1,7 @@
+const name = document.getElementById('name');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const error = document.getElementById('error');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
